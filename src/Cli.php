fec033acd526da0871c48cfda3290b764fdf3553
Marina Aldoshina <maldoshina@awg.ru>
2023-10-22 12:01:04 +0300

    fix tests errors

--- src/Cli.php
+++ src/Cli.php
@@ -9,6 +9,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use function cli\line;
 use function cli\prompt;
 
-line('Welcome to the Brain Game!');
+line('Welcome to the Brain Games!');
 $name = prompt('May I have your name?');
 line("Hello, %s!", $name);
